2dcb30dbea70ca1f2cf7865002a97cc0dd567966
cuongvu <cuonggvu97@gmail.com>
2020-03-31 17:32:18 +0700

    sua jquery scrollTop

--- 1.js
+++ 1.js
@@ -1,6 +1,23 @@
 ﻿$(document).ready(function() {
+  // alert("ok");
+  $("#crolltop").click(function() {
+    $('body,html').animate({ scrollTop: 1 }, 700);
+  });
 
-  $(".mouse").click(function() {
+  $("#mouse").click(function() {
     $('body,html').animate({ scrollTop: $('#khoiedu').offset().top }, 700);
   });
+
+  $("#gioithieu").click(function() {
+    $('body,html').animate({ scrollTop: $('#khoiedu').offset().top }, 700);
+  });
+
+  $("#comment").click(function() {
+    $('body,html').animate({ scrollTop: $('.cmt').offset().top }, 700);
+  });
+
+  $("#lienhe").click(function() {
+    $('body,html').animate({ scrollTop: $('.jumbotron').offset().top }, 700);
+  });
+
 });
